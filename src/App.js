b3801717b2d21e0bfcd61b73d54030822cca5b90
Joao Lucas <jlcarruda3@gmail.com>
2020-04-09 10:41:01 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+
+import Amplify, { Auth } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@aws-amplify/ui/dist/style.css';
+
+import awsconfig from './aws-exports';
 import './App.css';
 
-function App() {
+Amplify.configure(awsconfig)
+
+function App(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    const user = await Auth.signIn(email, password)
+    console.log(user.signInUserSession)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <FormLabel>Email</FormLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <FormLabel>Password</FormLabel>
+          <FormControl
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+        </FormGroup>
+        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+          Login
+        </Button>
+      </form>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, {usernameAttributes: 'email'});
